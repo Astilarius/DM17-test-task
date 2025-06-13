a8fc71cd14879c72f4ec9e1c96e54a1d3bf49e48
Astilarius <ruslanpronko1@gmail.com>
2025-06-13 20:04:40 +0300

    Set up project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import IndexPage from './pages/IndexPage.tsx'
+import { Provider } from "./components/ui/provider"
+import { Provider as ReduxProvider } from 'react-redux'
+import store from './store/store.ts'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage/>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+      <ReduxProvider store={store}>
+        <Provider>
+          <RouterProvider router={router} />
+        </Provider>
+      </ReduxProvider>
+    </StrictMode>,
+);
