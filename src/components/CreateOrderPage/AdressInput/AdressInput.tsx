3e059bb8f2abd917bf53446db60efdef2d898ba9
Astilarius <ruslanpronko1@gmail.com>
2025-06-17 19:49:32 +0300

    Add adress tooltip

--- src/components/CreateOrderPage/AdressInput/AdressInput.tsx
+++ src/components/CreateOrderPage/AdressInput/AdressInput.tsx
@@ -1,62 +1,86 @@
-import { Button, Field, Group, Icon, Input } from "@chakra-ui/react"
+import { Button, Field, Group, Icon, Input, Menu, Portal } from "@chakra-ui/react"
 import CopyIcon from  '../../../assets/FiCopy.svg'
+import { Tooltip } from "../../ui/tooltip"
 import type {InputComponentProps} from '../InputComponent/InputComponent.tsx'
 import {token} from '../../../token.ts'
 import './AdressInput.css'
+import { useState } from "react"
 
-// interface Suggestion {
-//     value: string
-// }
+interface Suggestion {
+  value: string,
+}
 
 function AdressInput(props:InputComponentProps) {
-    function inputAdress(e:React.ChangeEvent<HTMLInputElement>) {
-        props.setValue(e.currentTarget.value)
-        const apiUrl = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address ';
-        const data = {
-          "query": e.currentTarget.value,
-          "count": 5,
-        };
-        const requestOptions = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            "Authorization": `Token ${token}`
-          },
-          body: JSON.stringify(data),
-        };
-        fetch(apiUrl, requestOptions)
-          .then(response => response.json())
-          .then(result => console.log(result))
-      }
+  const [open, setOpen] = useState(false)
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([])
+  
+  function inputAdress(e:React.ChangeEvent<HTMLInputElement>) {
+    setOpen(value=>!value)
+    props.setValue(e.currentTarget.value)
+    const apiUrl = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address ';
+    const data = {
+      "query": e.currentTarget.value,
+      "count": 5,
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": `Token ${token}`
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(apiUrl, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        setSuggestions(result.suggestions)
+        console.log(result.suggestions)
+      })
+  }
 
-    return (
-    <Field.Root invalid={props.isError&&props.isRequired} required={props.isRequired} className="order-data__input-field-group">
+  function openTooltip(){
+    if (suggestions.length > 0) {
+      setOpen(true)
+    }
+  }
+
+  return (
+  <Field.Root invalid={props.isError&&props.isRequired} required={props.isRequired} className="order-data__input-field-group">
     <Field.Label className="order-data__input-field-title">
         {props.label}
     </Field.Label>
     <Group className="order-data__input-field">
-        <Input
-        value={props.value}
-        onChange={(e)=>inputAdress(e)}
-        />
-        {/* {suggestions.map(suggestion=>
-        <Menu.Item key={suggestion.value} value={suggestion.value}>{suggestion.value}</Menu.Item>
-        )} */}
-        <Button
-            colorPalette={'blue'} 
-            variant={'outline'} 
-            className="order-data__copy-button" 
-            onClick={()=>{
-                navigator.clipboard.writeText(String(props.value))
-            }}
+      <Tooltip open={open} content=
+      {<div className="order-data__adress-input-tooltip">
+        {suggestions.map(suggestion=>
+          <Button className="order-data__adress-input-tooltip-button" key={suggestion.value} onClick={()=>props.setValue(suggestion.value)}>{suggestion.value}</Button>
+        )}
+      </div>}
         >
-        <Icon>
+        <Input
+          onBlur={()=>setOpen(false)}
+          onFocus={openTooltip}
+          onPointerEnter={openTooltip}
+
+          value={props.value}
+          onChange={(e)=>inputAdress(e)}
+        />
+      </Tooltip>
+        <Button
+          colorPalette={'blue'} 
+          variant={'outline'} 
+          className="order-data__copy-button" 
+          onClick={()=>{
+              navigator.clipboard.writeText(String(props.value))
+          }}
+        >
+          <Icon>
             <img src={CopyIcon}/>
-        </Icon>
+          </Icon>
         </Button>
-    </Group>
-    </Field.Root>)
+      </Group>
+  </Field.Root>)
 }
 
 export default AdressInput
